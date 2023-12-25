7a1f280f4a7d4adb019614f01f6ed8c089aa6881
Niklas Ilves <niklas.ilves@advania.com>
2023-12-25 15:14:44 +0100

    Add install script sh

--- starship/InstallConfigureStarship.sh
+++ starship/InstallConfigureStarship.sh
@@ -1,6 +1,7 @@
 cd
-curl -sS https://starship.rs/install.sh | bash -s -- --yes
+curl -sS https://starship.rs/install.sh | sh
 
 echo 'eval "$(starship init bash)"' >> ~/.bashrc
 mkdir -p ~/.config && touch ~/.config/starship.toml
 curl https://raw.githubusercontent.com/niklasilves/TerminalTheme/main/starship/.starship/starship.toml -o ~/.config/starship.toml
+bash
