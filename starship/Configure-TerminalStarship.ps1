981174c9880a6020f85292feb9dceb05a09c7765
Niklas Ilves <niklas.ilves@advania.com>
2023-12-25 14:45:57 +0100

    Fixing donet sources

--- starship/Configure-TerminalStarship.ps1
+++ starship/Configure-TerminalStarship.ps1
@@ -1,10 +1,3 @@
-function SetTherminalGitPath {
-    if ($env:COMPUTERNAME -eq "ADSE-5CG2246HCG"){
-        set-location "C:\_data\.DevOps\AzDevOps\iLVES365\DevOps\DevOps\TerminalTheme\Starship-Win"
-    }else {
-        set-location "C:\Data\github\c-shark\Therminal"
-    }
-}
 function InstallAppsWinget {
     $Apps = 'Microsoft.PowerShell','git.git','Microsoft.VisualStudioCode','Microsoft.WindowsTerminal','starship'
     $apps | Foreach-Object { winget install $_ }
@@ -20,21 +13,21 @@ function InstallModules{
 }
 
 function InstallNerdFonts {
-    cd  $env:USERPROFILE
+    Set-Location  $env:USERPROFILE
     #Download Nerd Fonts , only root
     git clone --filter=blob:none --sparse https://github.com/ryanoasis/nerd-fonts.git
     
-    cd $env:USERPROFILE\nerd-fonts
+    Set-Location $env:USERPROFILE\nerd-fonts
     
     #Download CascadiaCode font
     git sparse-checkout add patched-fonts/Hack
     
-    #Install CascadiaCdode font
+    #Install Hack font
     ./install.ps1 Hack
 }
 
 function CreateDotFolders {
-    cd  $env:USERPROFILE
+    Set-Location  $env:USERPROFILE
     
     if(-not (Test-Path("$Env:USERPROFILE\.config"))){
         New-Item -Path "$Env:USERPROFILE\.config" -ItemType Directory -Verbose
@@ -42,36 +35,18 @@ function CreateDotFolders {
     if(-not (Test-Path("$Env:USERPROFILE\.config\powershell"))){
         New-Item -Path "$Env:USERPROFILE\.config\powershell" -ItemType Directory -Verbose
     }   
+    if(-not (Test-Path("$Env:USERPROFILE\.starship"))){
+        New-Item -Path "$Env:USERPROFILE\.starship" -ItemType Directory -Verbose
+    }   
 }
-
-<#
-
-#>
-
-SetTherminalGitPath
+CreateDotFolders
 New-Item $PROFILE -Force 
 Add-Content -Path $PROFILE -Value '[System.Console]::OutputEncoding = [System.Text.Encoding]::ASCII'
 Add-Content -Path $PROFILE -Value '$ENV:STARSHIP_CONFIG = "$HOME\.starship\starship.toml"'
-Add-Content -Path $PROFILE -Value '$ENV:STARSHIP_DISTRO = "者 xcad"'
 Add-Content -Path $PROFILE -Value 'Invoke-Expression (&starship init powershell)'
-Add-Content -Path $PROFILE -Value '. "$env:USERPROFILE\.config\powershell\user_vsCode_profile.ps1"'
+Add-Content -Path $PROFILE -Value '. "$env:USERPROFILE\.config\powershell\user_profile.ps1"'
 
-Copy-Item -Path $PSScriptRoot\.starship\starship.toml -Destination $HOME\.starship\starship.toml -Force -Verbose
-
-#Return to user profile folder
-SetTherminalGitPath
-
-#Add PowerShell Profile Script
-#Invoke-WebRequest -Uri https://raw.githubusercontent.com/canix1/TerminalTheme/main/Microsoft.VSCode_profile.ps1 -OutFile "$Env:USERPROFILE\\Documents\PowerShell\Microsoft.VSCode_profile.ps1" 
-Copy-Item $PSScriptRoot\.starship\starship.toml -Destination $Env:USERPROFILE\.starship\starship.toml -Force
-Copy-Item $PSScriptRoot\.config\user_profile.ps1 -Destination $Env:USERPROFILE\.config\powershell\user_profile.ps1 -Force
-Copy-Item $PSScriptRoot\.config\user_vsCode_profile.ps1 -Destination $Env:USERPROFILE\.config\powershell\user_vsCode_profile.ps1 -Force
-#Copy-Item $PSScriptRoot\Microsoft.VSCode_profile.ps1 -Destination $Env:USERPROFILE\Documents\PowerShell\Microsoft.VSCode_profile.ps1 -Force
-
-SetTherminalGitPath
-<#
-
-#Return to user profile folder
-cd  $env:USERPROFILE
-
-#>
+Invoke-WebRequest -Uri https://raw.githubusercontent.com/niklasilves/TerminalTheme/main/starship/.starship/starship.toml -OutFile "$Env:USERPROFILE\.starship\starship.toml"
+Invoke-WebRequest -Uri https://raw.githubusercontent.com/niklasilves/TerminalTheme/main/starship/.config/user_profile.ps1 -OutFile "$Env:USERPROFILE\.config\powershell\user_profile.ps1" 
+Invoke-WebRequest -Uri https://raw.githubusercontent.com/niklasilves/TerminalTheme/main/starship/.config/user_vsCode_profile.ps1 -OutFile "$Env:USERPROFILE\.config\powershell\user_vsCode_profile.ps1" 
+Set-Location $PSScriptRoot
