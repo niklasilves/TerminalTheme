dc33f30b0a0e74a29b7e9a6239e2244eb24d51fa
Niklas Ilves <niklas.ilves@advania.com>
2023-12-25 15:05:49 +0100

    Add install script sh

--- README.md
+++ README.md
@@ -1,4 +1,6 @@
 # TerminalTheme
 
 ## Install on linux
-''' curl -sS https://raw.githubusercontent.com/niklasilves/TerminalTheme/main/starship/InstallConfigureStarship.sh | sh
+``` 
+curl -sS https://raw.githubusercontent.com/niklasilves/TerminalTheme/main/starship/InstallConfigureStarship.sh | sh
+```
--- starship/InstallConfigureStarship.sh
+++ starship/InstallConfigureStarship.sh
@@ -1,7 +1,7 @@
+cd
+
 curl -sS https://starship.rs/install.sh | sh
-
-eval "$(starship init bash)"
-
+exit
+echo 'eval "$(starship init bash)"' >> ~/.bashrc
 mkdir -p ~/.config && touch ~/.config/starship.toml
-
-vi .config/starship.toml
+curl https://raw.githubusercontent.com/niklasilves/TerminalTheme/main/starship/.starship/starship.toml -o ~/.config/starship.toml
